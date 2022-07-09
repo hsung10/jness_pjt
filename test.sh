bb5e5a9a4f5ec8cd61b44e3253446e82760ace1f
hsung10 <hsung10@gmail.com>
2022-07-09 21:04:05 +0900

    test.sh_v1.1

--- test.sh
+++ test.sh
@@ -100,3 +100,16 @@ echo -n "숫자 B를 입력하세요: "
 read VAR2
 
 file_cmp
+
+
+if [ $VAR1 -eq $VAR2 ]; then
+    echo "숫자 A와 B는 같습니다."
+    else
+        if [ $VAR1 -ge $VAR2 ]; then
+            echo "숫자 A는 B보다 크거나 같습니다."
+            else
+                if [ $VAR1 -le $VAR2 ]; then
+                    echo "숫자 A는 B보다 작거나 같습니다."
+                fi
+        fi
+fi
